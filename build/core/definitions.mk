821f3d7307715788ff7edde4b93835cea0606957
Sahil Soni <sscsps@gmail.com>
2017-06-14 22:01:50 +0530

    step1

---
+++ build/core/definitions.mk
@@ -0,0 +1,2 @@
+LOCAL_PATH := $(call my-dir)
+MARVELL_CUSTOM_SHARED_LIBRARY := $(LOCAL_PATH)/shared_library.mk
---
+++ src/Android.mk
@@ -0,0 +1,3 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE := mycode
+include $(MARVELL_CUSTOM_SHARED_LIBRARY)
